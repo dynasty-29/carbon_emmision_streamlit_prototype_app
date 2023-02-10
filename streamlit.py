e4fa6c57bd84748596af8c4cf453335d99d7ac22
Sonnie <69596848+dynasty-29@users.noreply.github.com>
2023-02-10 17:33:36 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -49,30 +49,4 @@ if st.button("CO2 EMISSION PER CAPITA"):
       st.success('Below 25GT COe Continue reducing your CO2 Production by shifting to a decarbonization pathway')
 
 
-st.title('CO2 PER CAPITA Prediction ML App')
-st.markdown('A model that can predict the level of CO2 EMISSION PER CAPITA .')
-st.header('CO2 Emission Variables')
-col1, col2 = st.columns(2)
-
-
-# display the front end aspect
-# st.markdown(html_temp, unsafe_allow_html = True) 
-  
-# following lines create boxes in which user can enter data required to make prediction 
-with col1:
-    department = st.selectbox('Department',('sewing','finishing'))
-    quarter = st.number_input('Month Quarter', min_value = 1, step = 1, max_value = 4)
-    team = st.number_input('Team Number', min_value = 1, step = 1, max_value = 12)
-    targeted_productivity = st.number_input('Targeted Productivity', min_value = 0.00,  max_value = 10.00, step = 0.01)
-    std_minute_value = st.number_input('Standard Minute Value', min_value = 0.00, step = 0.01)
-
-with col2:
-    work_in_progress = st.number_input('Work In Progress', min_value = 0.00, step = 0.01)
-    over_time = st.number_input('Over Time', min_value = 0, step = 1)
-    incentive = st.number_input('Incentive', min_value = 0.00, step = 0.01)
-    idle_men = st.number_input('Idle Men', min_value = 0, step = 1)
-    no_of_workers = st.number_input('Number of Workers', min_value = 0, step = 1)
-
-result =""
-
 
